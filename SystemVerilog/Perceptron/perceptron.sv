4137ce3e0ccdd1166fbe499c619f50d4c2b1401a
Andrew Chen <109437047+AxC1271@users.noreply.github.com>
2025-08-04 11:11:25 -0700

    Update perceptron.sv

--- SystemVerilog/Perceptron/perceptron.sv
+++ SystemVerilog/Perceptron/perceptron.sv
@@ -1,15 +1,13 @@
 module perceptron (
-  input logic signed[7:0] input1,
-  input logic signed[7:0] input2,
-  input logic signed[7:0] weight1,
-  input logic signed[7:0] weight2,
+  input logic signed[7:0] pixel_intensity,
+  input logic signed[7:0] weight,
   input logic signed[7:0] bias,
   output logic result
 );
 
   logic signed[7:0] weighted_sum;
 
-  assign weighted_sum = (input1 * weight1) + (input2 * weight2) + bias;
+  assign weighted_sum = (weight * pixel_intensity) + bias;
   assign result = (weighted_sum > 0) ? 1 : 0;
   
 endmodule
