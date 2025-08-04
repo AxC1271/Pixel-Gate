5b94551f7ba12569ef0d966fca13c5b4e539fe36
Andrew Chen <109437047+AxC1271@users.noreply.github.com>
2025-08-04 14:36:09 -0700

    Create vga_controller.sv

---
+++ SystemVerilog/VGAController/vga_controller.sv
@@ -0,0 +1,10 @@
+module vga_controller (
+  input logic clk,
+  input logic rst,
+  input logic perceptron_output,
+  output logic[3:0] red,
+  output logic[3:0] green,
+  output logic[3:0] blue
+);
+
+endmodule
